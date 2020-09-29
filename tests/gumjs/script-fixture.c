6d4681cbcce3e8d61495a6e5e8f8a7c100fab130
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-09-29 22:55:00 +0200

    [WIP] Add QuickJS-based runtime

--- tests/gumjs/script-fixture.c
+++ tests/gumjs/script-fixture.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2019 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2010-2020 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  * Copyright (C) 2013 Karl Trygve Kalleberg <karltk@boblycat.org>
  *
  * Licence: wxWindows Library Licence, Version 3.1
@@ -10,6 +10,7 @@
 #include "gum-init.h"
 #include "gumdukscriptbackend.h"
 #include "guminspectorserver.h"
+#include "gumquickscriptbackend.h"
 #include "gumscriptbackend.h"
 #include "valgrind.h"
 
@@ -54,7 +55,8 @@
     gchar * path;                                                         \
                                                                           \
     path = g_strconcat ("/GumJS/Script/" SCRIPT_SUITE, group, #NAME "#",  \
-        GUM_DUK_IS_SCRIPT_BACKEND (fixture_data) ? "DUK" : "V8",          \
+        GUM_QUICK_IS_SCRIPT_BACKEND (fixture_data) ? "QJS" :              \
+            GUM_DUK_IS_SCRIPT_BACKEND (fixture_data) ? "DUK" : "V8",      \
         NULL);                                                            \
                                                                           \
     g_test_add (path,                                                     \
